37e3a47e56a2634850700da53f06fbec3fe9d6f3
stephanieyoon <bsd@BSDs-iMac.local>
2017-04-12 15:52:34 +0800

    esdafdasf

--- script.js
+++ script.js
@@ -11,29 +11,92 @@ var cscore = document.getElementById("cscore");
 var scoreyou = 0;
 var scorec = 0;
 var rpsbody = document.getElementById("rpsbody");
-var text = "Click on one of the three to begin";
 
 
-
+function getCompChoice() {
+	var choices = ['rock', 'paper', 'scissors'];
+	var compChoice = choices[Math.floor(Math.random()*choices.length)];
+	return compChoice;
+}
 
 rock.addEventListener('click', function () {
-	text = "You picked rock."
-	rpsbody.innerHTML = text
+	var computerChoice = getCompChoice();
+
+	if (computerChoice === "paper") {
+		rpsbody.innerHTML = 'You picked rock. Computer picked paper. You lose!';
+		scoreyou = scoreyou - 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec + 10;
+		cscore.innerHTML = scorec;
+		}
+
+	else if (computerChoice === "scissors") {
+		rpsbody.innerHTML = 'You picked rock. Computer picked scissors. You win!';
+		scoreyou = scoreyou + 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec - 10;
+		cscore.innerHTML = scorec;
+		}	
+
+	else {
+		rpsbody.innerHTML = "You picked rock. Computer picked rock. It's a tie!";
+		}
+
 });
 
 paper.addEventListener('click', function (){
-	text = "You picked paper."
-	rpsbody.innerHTML = text	
+	var computerChoice = getCompChoice();
+
+	if (computerChoice === "scissors") {
+		rpsbody.innerHTML = 'You picked paper. Computer picked scissors. You lose!';
+		scoreyou = scoreyou - 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec + 10;
+		cscore.innerHTML = scorec;
+		}
+
+	else if (computerChoice === "rock") {
+		rpsbody.innerHTML = 'You picked paper. Computer picked scissors. You win!';
+		scoreyou = scoreyou + 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec - 10;
+		cscore.innerHTML = scorec;
+		}	
+
+	else {
+		rpsbody.innerHTML = "You picked paper. Computer picked paper. It's a tie!";
+		}
 });
 
 scissors.addEventListener('click', function (){
-	text = "You picked scissors."
-	rpsbody.innerHTML = text	
+	var computerChoice = getCompChoice();
+
+	if (computerChoice === "rock") {
+		rpsbody.innerHTML = 'You picked scissors. Computer picked rock. You lose!';
+		scoreyou = scoreyou - 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec + 10;
+		cscore.innerHTML = scorec;
+		}
+
+	else if (computerChoice === "paper") {
+		rpsbody.innerHTML = 'You picked scissors. Computer picked paper. You win!';
+		scoreyou = scoreyou + 10;
+		yourscore.innerHTML = scoreyou;
+		scorec = scorec - 10;
+		cscore.innerHTML = scorec;
+		}	
+
+	else {
+		rpsbody.innerHTML = "You picked scissors. Computer picked scissors. It's a tie!";
+		}
 });
 
 reset.addEventListener('click', function () {
 	scoreyou = 0
+	yourscore.innerHTML = scoreyou;
 	scorec = 0
+	cscore.innerHTML = scorec;
 	text = "Click on one of the three to begin"
 	rpsbody.innerHTML = text
 })
